e0a53632897a6cc40cadfdf9206b3ad4fbe888a1
robyn <65057146+robynsdev@users.noreply.github.com>
2021-03-09 10:54:29 +1100

    Task 4 completed

--- src/app/components/child-app-four/child-app-four.component.ts
+++ src/app/components/child-app-four/child-app-four.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-child-app-four',
   templateUrl: './child-app-four.component.html',
-  styleUrls: ['./child-app-four.component.css']
+  styleUrls: ['./child-app-four.component.css'],
 })
 export class ChildAppFourComponent implements OnInit {
+  @Output() public outputText = new EventEmitter<string>();
 
-  constructor() { }
+  form: FormGroup;
+  private valueChange: Subscription;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      inputText: new FormControl(null),
+    });
+
+    const a = this.form.get('inputText');
+    this.valueChange = a.valueChanges.subscribe((value) =>
+      this.outputText.emit(value)
+    );
   }
 
+  ngOnDestroy() {
+    this.valueChange.unsubscribe();
+  }
 }
--- src/app/components/parent-app-four/parent-app-four.component.ts
+++ src/app/components/parent-app-four/parent-app-four.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-parent-app-four',
   templateUrl: './parent-app-four.component.html',
-  styleUrls: ['./parent-app-four.component.css']
+  styleUrls: ['./parent-app-four.component.css'],
 })
 export class ParentAppFourComponent implements OnInit {
+  inputText: string;
+  private valueChange: Subscription;
 
-  constructor() { }
+  messageEmitter = new BehaviorSubject<string>('');
 
-  ngOnInit(): void {
+  setMessage(message: string) {
+    this.messageEmitter.next(message);
   }
 
+  getMessage(): Observable<string> {
+    return this.messageEmitter.asObservable();
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.valueChange = this.getMessage().subscribe((message) => {
+      this.inputText = message;
+    });
+  }
+
+  ngOnDestroy() {
+    this.valueChange.unsubscribe();
+  }
 }
